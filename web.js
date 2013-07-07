20859293e917c26f12feb7c99e5d3a7c9998419d
Ubuntu <ubuntu@ip-172-31-35-82.us-west-2.compute.internal>
2013-07-07 08:45:24 +0000

    Added index.html and web.js changes error test

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
 	var fs = require('fs');
-	buf = fs.readFileSync("index.html");
+	buf = fs.readFileSync("index.htmle");
 	if (buf != null) {
 		response.send(buf.toString());
 	} else {
