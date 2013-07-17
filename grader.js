88b7767850a6dd3c88bea54dff7c9b31ba2361c8
Ubuntu <ubuntu@ip-172-31-35-82.us-west-2.compute.internal>
2013-07-17 16:53:54 +0000

    grader changes plus index change

--- grader.js
+++ grader.js
@@ -21,11 +21,14 @@ References:
    - https://developer.mozilla.org/en-US/docs/JSON#JSON_in_Firefox_2
 */
 
+var util = require('util');
+var rest = require('restler');
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URLFILE_DEFAULT = "http://www.google.com/";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -37,7 +40,7 @@ var assertFileExists = function(infile) {
 };
 
 var cheerioHtmlFile = function(htmlfile) {
-    return cheerio.load(fs.readFileSync(htmlfile));
+    return cheerio.load(htmlfile /*fs.readFileSync(htmlfile)*/);
 };
 
 var loadChecks = function(checksfile) {
@@ -61,14 +64,37 @@ var clone = function(fn) {
     return fn.bind({});
 };
 
+var checkJsonProcess = function(checkJson) {
+	var outJson = JSON.stringify(checkJson, null, 4);
+	console.log(outJson);
+};
+
+var responseFromRest = function(result, response) {
+	if (result instanceof Error) {
+		if (response) {
+			console.error('Error: ' + util.format(response.message));
+		} else {
+			console.error('Error: invalid URL');
+		}
+	} else {
+		checkJson = checkHtmlFile(result, program.checks);
+		checkJsonProcess(checkJson);
+	}
+};
+	
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
+        .option('-u, --url <check_url>', 'url of the file', "", URLFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+	var checkJson
+	if (program.url) {
+		rest.get(program.url).on('complete', responseFromRest);
+	} else	if (program.file) {
+		checkJson = checkHtmlFile(fs.readFileSync(program.file), program.checks);
+		checkJsonProcess(checkJson);
+	}
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
